cd36dc69a3fe2ffc45f07eb073c74738137883d8
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-11 17:10:53 +0100

    terraform

---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "env" {}
+variable "subnet_ids" {}
+variable "allow_cidr_blocks" {}
+variable "vpc_id" {}
+variable "num_node_groups" {}
+variable "node_type" {}
+variable "replicas_per_node_group" {}
